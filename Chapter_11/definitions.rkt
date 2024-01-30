6c1cb15368ae01a7d4029daeb79566402dbaa68e
josh-reynolds <josh.reynolds@frontier.com>
2024-01-30 12:32:23 -0800

    Chapter 11: Welcome Back to the Show - through p. 4

--- Chapter_11/definitions.rkt
+++ Chapter_11/definitions.rkt
@@ -8,3 +8,32 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      ((eq? a (car lat)) #t)
+      (else (member? a (cdr lat))))))
+; ------------------------------
+
+; ------------------------------
+(define two-in-a-row?
+  (lambda (lat)
+    (cond
+      ((null? (cdr lat)) #f)
+      ((eq? (car lat) (car (cdr lat))) #t)
+      (else (two-in-a-row? (cdr lat))))))
+; ------------------------------
+
+
+
+(define list1
+  (list 'Italian 'sardines 'spaghetti 'parsley))
+
+(define list2
+  (list 'Italian 'sardines 'sardines 'spaghetti 'parsley))
+
+(define list3
+  (list 'Italian 'sardines 'more 'sardines 'spaghetti 'parsley))
