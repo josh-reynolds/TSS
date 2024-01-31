63b34a595cf8f3f6dc107d710d2e97e143657c46
josh-reynolds <josh.reynolds@frontier.com>
2024-01-31 14:08:24 -0800

    Chapter 11: Welcome Back to the Show - through p. 7

--- Chapter_11/definitions.rkt
+++ Chapter_11/definitions.rkt
@@ -45,6 +45,41 @@
       (else (eq? a (car lat))))))
 ; ------------------------------
 
+; ------------------------------
+(define too-in-a-row?         ; revised version, with diff. helper
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else
+       (is-first-b? (car lat) (cdr lat))))))
+; ------------------------------
+
+; ------------------------------
+(define is-first-b?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? a (car lat))
+                (too-in-a-row? lat))))))
+; ------------------------------
+
+; ------------------------------
+(define two-in-a-row-b?
+  (lambda (preceding lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? preceding (car lat))
+                (two-in-a-row-b? (car lat) (cdr lat)))))))
+; ------------------------------
+
+; ------------------------------
+(define 2-in-a-row-b?
+  (lambda (lat)
+    (cond
+      ((null? lat) #f)
+      (else (two-in-a-row-b? (car lat) (cdr lat))))))
+; ------------------------------
+
 (define list1
   (list 'Italian 'sardines 'spaghetti 'parsley))
 
