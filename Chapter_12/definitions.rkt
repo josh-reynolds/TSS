741c7df3092792ddd28b28405354fa89c4f4f11f
josh-reynolds <josh.reynolds@frontier.com>
2024-02-01 13:49:00 -0800

    Chapter 12: Take Cover - through p. 17

--- Chapter_12/definitions.rkt
+++ Chapter_12/definitions.rkt
@@ -8,3 +8,37 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+;(define multirember
+;  (lambda (a lat)
+;    ((Y (lambda (mr)
+;          (lambda (lat)
+;            (cond
+;              ((null? lat) '())
+;              ((eq? a (car lat)) (mr (cdr lat)))
+;              (else (cons (car lat) (mr (cdr lat))))))))
+;     lat)))   
+; ------------------------------
+
+; ------------------------------
+;(define ???
+;  ((lambda (le)
+;     ((lambda (f) (f f))
+;        (lambda (f)
+;          (le (lambda (x) ((f f) x)))))
+;     (lambda (length)
+;       (lambda (l)
+;         (cond
+;           ((null? l) 0)
+;           (else (add1 (length (cdr l))))))))))
+; ------------------------------
+
+; ------------------------------
+;(define length
+;  (Y (lambda (length)
+;       (lambda (l)
+;         (cond
+;           ((null? l) 0)
+;           (else (add1 (length (cdr l)))))))))
+; ------------------------------
