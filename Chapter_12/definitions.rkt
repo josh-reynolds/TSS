6ecbd51563cf0cab3e167817183ed57458e6ba35
josh-reynolds <josh.reynolds@frontier.com>
2024-02-02 15:53:21 -0800

    Chapter 12: Take Cover - through p. 23

--- Chapter_12/definitions.rkt
+++ Chapter_12/definitions.rkt
@@ -84,6 +84,30 @@
           (mr lat))))
 ; ------------------------------
 
+; ------------------------------
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) '())
+        ((test? a (car l)) (cdr l))
+        (else (cons (car l) ((rember-f test?) a (cdr l))))))))
+; ------------------------------
+
+; ------------------------------
+(define rember-eq? (rember-f eq?))
+; ------------------------------
+
+; ------------------------------
+(define multirember-f
+  (lambda (test?)
+    (lambda (a lat)
+      (cond
+        ((null? lat) '())
+        ((test? a (car lat)) ((multirember-f test?) a (cdr lat)))
+        (else (cons (car lat) ((multirember-f test?) a (cdr lat))))))))
+; ------------------------------
+
 
 (define list1
   (list 'apple 'custard 'pie 'linzer 'pie 'torte))
