af8e610e564857e017ad2b62cfb4cde5fee9b5d5
josh <josh.reynolds@frontier.com>
2024-02-06 20:09:03 -0800

    Chapter 13: Hop, Skip, and Jump - through p. 49

--- Chapter_13/definitions.rkt
+++ Chapter_13/definitions.rkt
@@ -127,6 +127,31 @@
         (else (I set1))))))
 ; ------------------------------
 
+; ------------------------------
+(define intersectall7         ; text reversed the member?/else clauses
+  (lambda (lset)              ; in J which is different from intersect3 above
+    (let/cc hop               ; and seems like a bug in the book...
+      (letrec
+          ((A (lambda (lset)
+                (cond
+                  ((null? (car lset)) (hop '()))
+                  ((null? (cdr lset)) (car lset))
+                  (else (I (car lset) (A (cdr lset)))))))
+           (I (lambda (s1 s2)
+                (letrec
+                    ((J (lambda (s1)
+                         (cond
+                           ((null? s1) '())
+                           ((member? (car s1) s2) (cons (car s1) (J (cdr s1))))
+                           (else (J (cdr s1)))))))
+                  (cond
+                    ((null? s2) '())
+                    (else (J s1)))))))
+        (cond
+          ((null? lset) '())
+          (else (A lset)))))))
+; ------------------------------
+
 (define set1
   (list 'tomatoes 'and 'macaroni))
 
