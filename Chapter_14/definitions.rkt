54d940f84bd3eec88836f98a5afbea67ed760452
josh <josh.reynolds@frontier.com>
2024-02-08 20:46:06 -0800

    Chapter 14: Let There Be Names - through p. 63

--- Chapter_14/definitions.rkt
+++ Chapter_14/definitions.rkt
@@ -8,3 +8,28 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define leftmost
+  (lambda (l)
+    (cond
+      ((atom? (car l)) (car l))
+      (else (leftmost (car l))))))
+; ------------------------------
+
+
+(define list1
+  (list (list (list 'a)
+              'b)
+        (list 'c 'd)))
+
+(define list2
+  (list (list (list 'a)
+              '())
+        '()
+        (list 'e)))
+
+(define list3
+  (list (list (list '()
+                    'a)
+              '())))
