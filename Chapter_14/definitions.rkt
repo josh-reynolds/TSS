fd325f8301d861c02b8695e5469332b1026d61d9
josh-reynolds <josh.reynolds@frontier.com>
2024-02-09 12:38:55 -0800

    Chapter 14: Let There Be Names - through p. 67

--- Chapter_14/definitions.rkt
+++ Chapter_14/definitions.rkt
@@ -10,6 +10,34 @@
 ; ------------------------------
 
 ; ------------------------------
+(define o-equal?
+  (lambda (s1 s2)
+    (cond
+      ((and (atom? s1)(atom? s2)) (equan? s1 s2))
+      ((or (atom? s1)(atom? s2)) #f)
+      (else (eqlist? s1 s2)))))
+; ------------------------------
+
+; ------------------------------
+(define eqlist?
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1)(null? l2)) #t)
+      ((or (null? l1)(null? l2)) #f)
+      (else (and (o-equal? (car l1)(car l2))
+                 (o-equal? (cdr l1)(cdr l2)))))))
+; ------------------------------
+
+; ------------------------------
+(define equan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1)(number? a2))(= a1 a2))
+      ((or (number? a1)(number? a2)) #f)
+      (else (eq? a1 a2)))))
+; ------------------------------
+
+; ------------------------------
 (define leftmost
   (lambda (l)
     (cond
@@ -42,6 +70,41 @@
            (else (leftmost3 (cdr l)))))))))
 ; ------------------------------
 
+; ------------------------------
+(define rember1*
+  (lambda (a l)
+    (cond
+      ((null? l) '())
+      ((atom? (car l))
+       (cond
+         ((eq? a (car l)) (cdr l))
+         (else (cons (car l) (rember1* a (cdr l))))))
+      (else
+       (cond
+         ((eqlist? (rember1* a (car l)) (car l))
+          (cons (car l) (rember1* a (cdr l))))
+         (else (cons (rember1* a (car l)) (cdr l))))))))
+; ------------------------------
+
+; ------------------------------
+(define rember1*2
+  (lambda (a l)
+    (letrec
+        ((R (lambda (l)
+              (cond
+                ((null? l) '())
+                ((atom? (car l))
+                 (cond
+                   ((eq? a (car l)) (cdr l))
+                   (else (cons (car l) (R (cdr l))))))
+                (else
+                 (cond
+                   ((eqlist? (R (car l)) (car l))
+                    (cons (car l) (R (cdr l))))
+                   (else (cons (R (car l)) (cdr l)))))))))
+         (R l))))
+; ------------------------------
+
 (define list1
   (list (list (list 'a)
               'b)
@@ -57,3 +120,15 @@
   (list (list (list '()
                     'a)
               '())))
+
+(define list4
+  (list (list 'Swedish 'rye)
+        (list 'French (list 'mustard 'salad 'turkey))
+        'salad))
+
+(define list5
+  (list (list 'pasta 'meat)
+        'pasta
+        (list 'noodles 'meat 'sauce)
+        'meat
+        'tomatoes))
