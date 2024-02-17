3b63492c5d064387e736c8eb9b42d3c710ec8d82
josh-reynolds <josh.reynolds@frontier.com>
2024-02-17 13:04:23 -0800

    Chapter 17: We Change, Therefore We Are! - through p. 127

--- Chapter_17/definitions.rkt
+++ Chapter_17/definitions.rkt
@@ -8,3 +8,46 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define deep
+  (lambda (m)
+    (if (zero? m)
+        'pizza
+        (cons (deep (sub1 m))
+              '()))))
+; ------------------------------
+
+; ------------------------------
+(define find
+  (lambda (n Ns Rs)
+    (letrec
+        ((A (lambda (ns rs)
+              (cond
+                ((null? ns) #f)
+                ((= (car ns) n) (car rs))
+                (else (A (cdr ns) (cdr rs)))))))
+         (A Ns Rs))))
+; ------------------------------
+
+; typo on p. 127? they have 'RS' in the exists/find line
+; instead of 'Rs' - correcting here
+; ------------------------------
+(define deepM
+  (let ((Rs '())
+        (Ns '()))
+    (letrec
+        ((D (lambda (m)
+              (if (zero? m)
+                  'pizza
+                  (cons (deepM (sub1 m))
+                        '())))))
+      (lambda (n)
+        (let ((exists (find n Ns Rs)))
+          (if (atom? exists)
+              (let ((result (D n)))
+                (set! Rs (cons result Rs))
+                (set! Ns (cons n Ns))
+                result)
+              exists))))))
+; ------------------------------
