9a9d212a26b7440c054bc8524f1057cf6fb27f53
josh <josh.reynolds@frontier.com>
2024-02-21 23:27:22 -0500

    Chapter 18: We Change, Therefore We Are the Same! - through p. 143

--- Chapter_18/definitions.rkt
+++ Chapter_18/definitions.rkt
@@ -9,3 +9,19 @@
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
 
+; ------------------------------
+(define lots                  ; they use "kons" instead of "cons"
+  (lambda (m)
+    (cond
+      ((zero? m) '())
+      (else (cons 'egg
+                  (lots (sub1 m)))))))
+; ------------------------------
+
+; ------------------------------
+(define lenkth                ; they use "kdr" instead of "cdr"
+  (lambda (l)
+    (cond
+      ((null? l) 0)
+      (else (add1 (lenkth (cdr l)))))))
+; ------------------------------
